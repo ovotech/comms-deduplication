d30cef8dc50adacd223b0abf9b7b5a94c8c32a0a
Filippo De Luca <filippo.deluca@ovoenergy.com>
2020-08-17 13:25:59 +0200

    add more test around startProcessing

--- src/test/scala/deduplication/DeduplicationSuite.scala
+++ src/test/scala/deduplication/DeduplicationSuite.scala
@@ -1,151 +1,122 @@
 package com.ovoenergy.comms.deduplication
 
-import java.{util => ju}
 import java.time.Instant
+import java.{util => ju}
+import java.util.UUID
 
+import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
 import cats.implicits._
+import cats.effect._
+import cats.effect.laws.util.TestContext
 
-import org.scalacheck.Prop._
+import munit._
+import org.scalacheck.Arbitrary
 
 import model._
-import Generators._
+import Config._
 
-class DeduplicationSuite extends munit.ScalaCheckSuite {
+class DeduplicationSuite extends FunSuite {
 
-  property(
-    "processStatus should return Completed if the completedAt is present and expiredOn is None"
-  ) {
-    forAll { process: Process[ju.UUID, ju.UUID] =>
+  private val testContext = TestContext()
 
-      val now = Instant.now()
-      val maxProcessingTime = 5.minutes
+  implicit val ec: ExecutionContext = testContext
+  implicit val contextShift: ContextShift[IO] = testContext.ioContextShift
+  implicit val timer: Timer[IO] = testContext.ioTimer
 
-      val sample = process.copy(
-        completedAt = now.some,
-        expiresOn = None
-      )
+  override def munitValueTransforms = super.munitValueTransforms ++ List(
+    new ValueTransform("IO", {
+      case io: IO[_] => io.unsafeToFuture()
+    })
+  )
 
-      val status = Deduplication.processStatus(maxProcessingTime, now)(sample)
+  def given[A: Arbitrary]: A = Arbitrary.arbitrary[A].sample.get
 
-      assertEquals(clue(status), ProcessStatus.Completed)
-    }
+  class MockProcessRepo extends ProcessRepo[IO, UUID, UUID] {
+    def startProcessingUpdate(
+        id: UUID,
+        processorId: ju.UUID,
+        now: Instant
+    ): IO[Option[Process[UUID, UUID]]] = none[Process[UUID, UUID]].pure[IO]
+    def completeProcess(id: UUID, processorId: UUID, now: Instant, ttl: FiniteDuration): IO[Unit] =
+      IO.unit
   }
 
-  property(
-    "processStatus should return Completed if the completedAt is present and expiredOn is in the future"
+  test(
+    "tryStartProcess should return New when the process with the given ID has never started before"
   ) {
-    forAll { process: Process[ju.UUID, ju.UUID] =>
 
-      val now = Instant.now()
-      val maxProcessingTime = 5.minutes
+    val processId = given[UUID]
+    val processorId = given[UUID]
 
-      val sample = process.copy(
-        completedAt = now.some,
-        expiresOn = Expiration(now.plusMillis(1)).some
+    val deduplication = Deduplication[IO, UUID, UUID](
+      new MockProcessRepo {
+        override def startProcessingUpdate(
+            id: UUID,
+            processorId: ju.UUID,
+            now: Instant
+        ): IO[Option[Process[UUID, UUID]]] = none[Process[UUID, UUID]].pure[IO]
+      },
+      Config(
+        processorId = processorId,
+        maxProcessingTime = 5.minutes,
+        ttl = 30.days,
+        pollStrategy = PollStrategy.linear()
       )
+    )
 
-      val status = Deduplication.processStatus(maxProcessingTime, now)(sample)
+    deduplication
+      .flatMap { deduplication =>
+        deduplication.tryStartProcess(processId)
+      }
+      .map { outcome =>
+        assert(outcome.isInstanceOf[Outcome.New[IO]], clue(outcome))
+      }
 
-      assertEquals(clue(status), ProcessStatus.Completed)
-    }
   }
 
-  property("processStatus should return Expired if the expiresOn is present and it is in the past") {
-    forAll { process: Process[ju.UUID, ju.UUID] =>
-
-      val now = Instant.now()
-      val maxProcessingTime = 5.minutes
-
-      val sample = process.copy(
-        expiresOn = Expiration(now.minusMillis(1)).some
-      )
-
-      val status = Deduplication.processStatus(maxProcessingTime, now)(sample)
-
-      assertEquals(clue(status), ProcessStatus.Expired)
-    }
-  }
-
-  property(
-    "processStatus should return Timeout if startedAt is more than maxProcessingTime in the past, completedAt and expiresOn are None"
+  test(
+    "tryStartProcess should return Duplicate when the process with the given ID has already completed"
   ) {
-    forAll { process: Process[ju.UUID, ju.UUID] =>
 
-      val now = Instant.now()
-      val maxProcessingTime = 5.minutes
+    val processId = given[UUID]
+    val processorId = given[UUID]
 
-      val sample = process.copy(
-        startedAt = now.minusMillis(maxProcessingTime.toMillis).minusMillis(1),
-        completedAt = None,
-        expiresOn = None
+    val deduplication = Deduplication[IO, UUID, UUID](
+      new MockProcessRepo {
+        override def startProcessingUpdate(
+            id: UUID,
+            processorId: ju.UUID,
+            now: Instant
+        ): IO[Option[Process[UUID, UUID]]] = {
+          Deduplication.nowF[IO].map { now =>
+            Process(
+              id = id,
+              processorId = processorId,
+              startedAt = now.minusMillis(750),
+              completedAt = now.minusMillis(250).some,
+              expiresOn = None
+            ).some
+          }
+
+        }
+      },
+      Config(
+        processorId = processorId,
+        maxProcessingTime = 5.minutes,
+        ttl = 30.days,
+        pollStrategy = PollStrategy.linear()
       )
+    )
 
-      val status = Deduplication.processStatus(maxProcessingTime, now)(sample)
-
-      assertEquals(clue(status), ProcessStatus.Timeout)
-    }
-  }
-
-  property(
-    "processStatus should return Timeout if startedAt is more than maxProcessingTime in the past, expiresOn is in the future and completedAt is None"
-  ) {
-    forAll { process: Process[ju.UUID, ju.UUID] =>
-
-      val now = Instant.now()
-      val maxProcessingTime = 5.minutes
-
-      val sample = process.copy(
-        startedAt = now.minusMillis(maxProcessingTime.toMillis).minusMillis(1),
-        completedAt = None,
-        expiresOn = Expiration(now.plusMillis(1)).some
-      )
-
-      val status = Deduplication.processStatus(maxProcessingTime, now)(sample)
-
-      assertEquals(clue(status), ProcessStatus.Timeout)
-    }
-  }
-
-  property(
-    "processStatus returns running if startedAt is less than maxProcessingTime in the past, completedAt and expiresOn are None"
-  ) {
-    forAll { process: Process[ju.UUID, ju.UUID] =>
-
-      val now = Instant.now()
-      val maxProcessingTime = 5.minutes
-
-      val sample = process.copy(
-        startedAt = now.minusMillis(maxProcessingTime.toMillis).plusMillis(1),
-        completedAt = None,
-        expiresOn = None
-      )
-
-      val status = Deduplication.processStatus(maxProcessingTime, now)(sample)
-
-      assertEquals(clue(status), ProcessStatus.Running)
-    }
-  }
-
-  property(
-    "processStatus returns running if startedAt is less than maxProcessingTime in the past,  expiresOn is in the future and completedAt is None"
-  ) {
-    forAll { process: Process[ju.UUID, ju.UUID] =>
-
-      val now = Instant.now()
-      val maxProcessingTime = 5.minutes
-
-      val sample = process.copy(
-        startedAt = now.minusMillis(maxProcessingTime.toMillis).plusMillis(1),
-        completedAt = None,
-        expiresOn = Expiration(now.plusMillis(1)).some
-      )
-
-      val status = Deduplication.processStatus(maxProcessingTime, now)(sample)
-
-      assertEquals(clue(status), ProcessStatus.Running)
-    }
+    deduplication
+      .flatMap { deduplication =>
+        deduplication.tryStartProcess(processId)
+      }
+      .map { outcome =>
+        assertEquals(outcome, Outcome.Duplicate[IO]())
+      }
   }
 
 }
